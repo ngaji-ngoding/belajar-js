ab4e2fd3bc02f483d99b91296436c7ec2249da17
ngaji-ngoding <mahsun.b321@gmail.com>
2022-09-05 16:45:37 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+let menuSatu = "kopi hitam";
+var menuDua = "susu jahe";
+let menuTiga = "kopi susu";
+
+const title1 = document.querySelector("#m1");
+const title2 = document.querySelector("#m2");
+const title3 = document.querySelector("#m3");
+
+console.log(title1);
+title1.textContent = menuSatu;
+title2.textContent = menuDua;
+title3.textContent = menuTiga;
